8ae5034c1e53fa15c16af7cd015dde49c64501a8
Lonnie <lonnierod11@gmail.com>
2020-11-06 23:48:22 -0500

    Completed ChallengeSet05

--- ChallengeSets/ChallengeSet05.cs
+++ ChallengeSets/ChallengeSet05.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 
 namespace ChallengeSets
 {
@@ -7,37 +9,119 @@ namespace ChallengeSets
     {
         public int GetNextNumberDivisibleByN(int startNumber, int n)
         {
-            throw new NotImplementedException();
+            return ((startNumber / n) + 1) * n;
         }
 
         public void ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED(Business[] businesses)
         {
-            throw new NotImplementedException();
+          foreach (Business biz in businesses)
+            {
+                if (biz.TotalRevenue <= 0)
+                {
+                    biz.Name = "CLOSED";
+                }
+            }
         }
 
         public bool IsAscendingOrder(int[] numbers)
         {
-            throw new NotImplementedException();
+           if (numbers == null || numbers.Length == 0)
+            {
+                return false;
+            }
+
+                int previousNum = numbers[0];
+                for (int i = 1; i < numbers.Length; i++)
+                {
+                    if (numbers[i] < previousNum)
+                    {
+                    return false;
+                    }
+                previousNum = numbers[i];  
+                }
+            return true;
         }
 
         public int SumElementsThatFollowAnEven(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Length == 0)
+            {
+                return 0;
+            }
+            int sumNextEven = 0;
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i - 1] % 2 == 0)
+                {
+                   sumNextEven += numbers[i];
+                }
+            }
+            return sumNextEven;
         }
+
 
         public string TurnWordsIntoSentence(string[] words)
         {
-            throw new NotImplementedException();
+            if (words == null || words.Length == 0 || words.Length == 1)
+            {
+                return "";
+            }
+            string sentence = "";
+
+            foreach (var word in words)
+            {
+                if (word.Trim().Length > 0)
+                {
+                    sentence += word.Trim() + " ";
+                }
+            }
+                if (sentence.Length == 0)
+                {
+                return "";
+                }
+            sentence = sentence.Substring(0, sentence.Length - 1);
+            sentence += ".";
+            return sentence;
         }
 
         public double[] GetEveryFourthElement(List<double> elements)
         {
-            throw new NotImplementedException();
+           if (elements == null)
+            {
+                return new double[0];
+            }
+            List<double> everyFourth = new List<double>();
+            for (int i = 1; i <= elements.Count; i++)
+            {
+                if (i % 4 == 0)
+                {
+                    everyFourth.Add(elements[i - 1]);
+                }
+            }
+            return everyFourth.ToArray();
         }
 
         public bool TwoDifferentElementsInArrayCanSumToTargetNumber(int[] nums, int targetNumber)
         {
-            throw new NotImplementedException();
+            if (nums.Length == 0 || nums.Length == 1)
+            {
+                return false;
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+                    if (nums[i] + nums[j] == targetNumber)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
     }
 }
